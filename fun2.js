573d9dd1a7c254b943f30e5164aadde5ab06041c
amicoding <mdsagarkhan16656@gmail.com>
2024-10-26 17:02:23 +0600

    5 js problem solved

---
+++ fun2.js
@@ -0,0 +1,86 @@
+
+// problem 1: compare string is equal or unequal?
+
+let str1 = "it is a String one";
+let str2 = "it is a String one ";
+
+function comparString(a , b){
+  
+  return a === b ;
+}
+
+const sendStr = comparString(str1, str2);
+
+console.log(sendStr);
+
+// ******************************
+   
+  // problem 2: a function can cheak and find word in a sentence
+  
+  let sentence = "there are more programming language in our modern world. such c++, python, java, javascript etc";
+   
+   function findWord(text){
+     
+    return text.includes("javascript"); 
+    
+    // search here !!
+     
+   }
+   
+  let yourWord = findWord(sentence);
+  
+  console.log(yourWord);
+   
+// ******************************
+   
+  // problem 3: split method. simple sentence to array and array to sentence
+  
+  let text1 = "it is a sample text. here i use split method. after using this method text convert into array.";
+  
+   function textArrayConverter(text){
+     
+     let textToArray = text.split(" ");
+     
+     let arrayToText = textToArray.join("-");
+     
+      return textToArray + " == " + arrayToText;
+     
+   }
+   
+   let converter =  textArrayConverter(text1);
+  console.log(converter);
+   
+// ******************************
+   
+  // problem 4: a function return slice part of a sentence
+  
+  let text2 ="a function will take text and two index Number by 3 parameter. then the function return the slice part of the text";
+  
+  // console.log(text2.slice(0,7));
+  
+  function sliceText(txt, in1, in2){
+    
+    return txt.slice(in1, in2);
+    
+  }
+   
+  let part = sliceText(text2, 0, 30)
+  // give yor input 
+  
+  console.log(" your slice part : " +  part);
+// ******************************
+   
+  // problem 5: destructuring property from Object
+  
+  const person = {
+    
+    name : 'tariq',
+    email : 'tariq@gmail.com',
+    age : 22,
+  };
+   
+   let{name, age, email} = person;
+   
+   console.log('His name is ' + name + '.' + ' He is ' + age + ' years old, ' + 'His email address is : ' + email);
+   
+// ******************************
